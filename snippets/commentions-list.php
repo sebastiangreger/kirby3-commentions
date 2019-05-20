b1a3e2fc0b5e997707fe3132884df0e5f0c37569
sebastiangreger <sebastiangreger@users.noreply.github.com>
2019-05-20 13:53:30 +0200

    Format comments and webmentions

--- snippets/commentions-list.php
+++ snippets/commentions-list.php
@@ -4,30 +4,73 @@
 		<?php if ( $page->comments()->toStructure()->count() > 0 ) : ?>
 		
 		   <h3>Comments</h3>
-
-		   <?php foreach ( $page->comments()->toStructure() as $comment ) :
 		   
-				if ($comment->approved() != "true")
-					continue;
-				
-				?>
-				
-				<div id="<?= 'comment-' . $comment->number() ?>">
-					<h4>
-						<?php if ( $comment->type() == 'webmention' ) : ?><a href="<?= $comment->website() ?>"><?php endif; ?>
-						<?= htmlspecialchars( $comment->name() ) ?>
-						<?php if ( $comment->type() == 'webmention' ) : ?></a><?php endif; ?>
-					</h4>
-					<p>
-						<?= $comment->timestamp() ?>
-						<?php if ( $comment->type() == 'webmention' ) : ?>
-						(webmention from: <a href="<?= $comment->source() ?>"><?= $comment->source() ?></a>)
-						<?php endif; ?>
-					</p>
-					<?= $comment->message()->html() ?>
-			   </div>
+		   <ul>
 
-		   <?php endforeach; ?>
+			   <?php foreach ( $page->comments()->toStructure() as $comment ) :
+			   
+					if ($comment->approved() != "true")
+						continue;
+					
+					?>
+
+					<li class="commentions-list-type-<?= $comment->type() ?>">
+
+						<h4>
+
+							<?php
+							if ( htmlspecialchars( $comment->name() ) != '' )
+								$name = htmlspecialchars( $comment->name() );
+							else
+								$name = 'Anonymous';
+							?>
+
+							<?php if ( $comment->website() != '' ) : ?>
+								<a href="<?= $comment->website() ?>"><?= $name ?></a>
+							<?php else : ?>
+								<?= $name ?>
+							<?php endif;
+							
+							$domain = str_replace( 'www.', '', parse_url($comment->source(), PHP_URL_HOST) );
+							
+							switch ( $comment->type() ) {
+								case 'webmention':
+								case 'mention':
+									echo 'mentioned this at <a href="' . $comment->source() . '">' . $domain . '</a>';
+									break;
+								case 'like':
+									echo 'liked this at <a href="' . $comment->source() . '">' . $domain . '</a>';
+									break;
+								case 'bookmark':
+									echo 'bookmarked this at <a href="' . $comment->source() . '">' . $domain . '</a>';
+									break;
+								case 'reply':
+									echo 'replied at <a href="' . $comment->source() . '">' . $domain . '</a>:';
+									break;
+								default:
+									echo ":";
+									break;
+							}
+						
+							?>
+						
+						</h4>
+
+						<p class="commentions-list-date">
+							<?= date ( 'Y-m-d H:i', strtotime( $comment->timestamp() ) ) ?>
+						</p>
+
+						<?php if ( $comment->type() == 'reply' ) : ?>
+						<div class="commentions-list-message">
+							<?= $comment->message()->kt() ?>
+						</div>
+						<?php endif; ?>
+
+				   </li>
+
+			   <?php endforeach; ?>
+
+			</ul>
 
 		<?php endif; ?>
 
