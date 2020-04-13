eb310e268c421c4e24fc082a7819f74c2ff9a9e1
sebastiangreger <sebastiangreger@users.noreply.github.com>
2020-04-13 13:33:59 +0200

    Bugfix

--- config/routes.php
+++ config/routes.php
@@ -7,7 +7,7 @@ use Kirby\Http\Response;
 return [
 
 	[
-		'pattern' => option( 'sgkirby.commentions.endpoint' ),
+		'pattern' => option( 'sgkirby.commentions.endpoint', 'webmention-endpoint' ),
 		'method'  => 'GET|POST',
 		'action'  => function () { return Endpoint::route(); }
 	],
