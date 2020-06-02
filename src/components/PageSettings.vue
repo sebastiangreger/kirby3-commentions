2d350fb779318bc1948f160260500063dccbaf1f
sebastiangreger <sebastiangreger@users.noreply.github.com>
2020-06-02 09:32:08 +0200

    Change responsive grid settings

--- src/components/PageSettings.vue
+++ src/components/PageSettings.vue
@@ -14,6 +14,14 @@ export default {
 .k-commentions-pagesettings {
   margin-top: .5rem;
 }
+@media screen and (min-width:45em) {
+  .k-commentions-pagesettings {
+    grid-template-columns: repeat(var(--columns),1fr);
+  }
+  .k-column[data-width="1/3"], .k-column[data-width="2/6"] {
+    grid-column-start: span 4;
+  }
+}
 .k-commentions-pagesettings .k-input[data-theme="field"] {
   border: none;
   background: transparent;
